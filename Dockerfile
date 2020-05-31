1d8fcad51eed1d678e2becef04962357118ba488
tomoyasu-sano <anytimes.sano@gmail.com>
2020-05-31 22:57:30 +0900

    change db sqlite

--- Dockerfile
+++ Dockerfile
@@ -11,5 +11,3 @@ COPY requirements.txt /code/
 RUN pip install -r requirements.txt
 
 COPY . /code/
-
-CMD gunicorn --bind 0.0.0.0:$PORT wsgi 
--- django_docker_heroku_first/settings_dev.py
+++ django_docker_heroku_first/settings_dev.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
-
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,8 +25,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r+si17=fb1-^-##ms$+t#u-sb*ipbj^w&jen3bm2$2cjl8tx^p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -80,22 +78,14 @@ WSGI_APPLICATION = 'django_docker_heroku_first.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres_hoge',
-        'USER': 'postgres_hoge',
-        'HOST': 'db',
-        'PORT': 5433
-    }
-}
-"""
 
 
 # Password validation
@@ -135,16 +125,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-
-"""
-DEBUG = False
-try:
-    from .local_settings import *
-except ImportError:
-    pass
-if not DEBUG:
-    import django_heroku
-    django_heroku.settings(locals())
-"""
