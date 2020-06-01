144910716819a702903f930d38e7993746da945d
tomoyasu-sano <anytimes.sano@gmail.com>
2020-06-01 23:02:55 +0900

    edit wsgi

--- django_docker_heroku_first/wsgi.py
+++ django_docker_heroku_first/wsgi.py
@@ -13,7 +13,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 import sys
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_docker_heroku_first.settings")
 
